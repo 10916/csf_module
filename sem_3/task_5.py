970c9f75f91a95e38df82c8475d4f94fb865bee2
10916 <wiut10916@gmail.com>
2021-10-28 17:40:14 +0500

    added third seminar tasks

---
+++ sem_3/task_5.py
@@ -0,0 +1,11 @@
+dec = input("Type a decimal: ")
+
+int = int(dec)
+bin = bin(int)
+oct = oct(int)
+hex = hex(int)
+
+print("Integer form: " + dec)
+print("Binary form: " + bin)
+print("Octal form: " + oct)
+print("Hexadecimal form: " + hex)
